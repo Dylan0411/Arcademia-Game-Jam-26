5c4a829593d77039918cf4e72e00e8572574cf08
Dylan0411 <110481431+Dylan0411@users.noreply.github.com>
2026-02-27 18:33:47 +0000

    basic main menu made

---
+++ Assets/fishingMiniGame/fishCounter.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class fishCounter : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+
+        // attach this to bucket.. +1 to counter when fish hits it.
+        // when fish counter is => x then make the next fish caught the relic.
+        // when counter has been added to then spawn splash effect
+
+
+
+    }
+}
---
+++ Assets/fishingMiniGame/playerRodControl.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class playerRodControl : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+
+        // randomly bwteeen 2-5 seconds the rod gets pulled down.. player pulls back on rod and holds a to reel in fish
+        // when rod is fully reeled.. player uses right stick to move fish to bucket.
+        // when fish in bucket.. move rod back to default position.
+
+
+
+
+
+    }
+}
