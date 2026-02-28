e2aa452c0f336329d711e8d88ae89dadeba90d7d
Dylan0411 <110481431+Dylan0411@users.noreply.github.com>
2026-02-28 04:08:12 +0000

    adwwaq\D

--- Assets/fishingMiniGame/fishBucketCounter.cs
+++ Assets/fishingMiniGame/fishBucketCounter.cs
@@ -27,6 +27,19 @@ public class fishBucketCounter : MonoBehaviour
     public GameObject fishMovingUpStartPos;
     public GameObject fishMovingUpEndPos;
 
+    public GameObject skyFish;
+
+    public GameObject skyFishSpawnLocation1;
+    public GameObject skyFishSpawnLocation2;
+    public GameObject skyFishSpawnLocation3;
+    public GameObject skyFishSpawnLocation4;
+    public GameObject skyFishSpawnLocation5;
+    public GameObject skyFishSpawnLocation6;
+    public GameObject skyFishSpawnLocation7;
+    public GameObject skyFishSpawnLocation8;
+
+    public bool fadeScreen;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -37,11 +50,22 @@ public class fishBucketCounter : MonoBehaviour
         relic.transform.position = relicStartPos.transform.position;
         cursedLootUI.SetActive(false);
 
+        fishMovingUp.transform.position = fishMovingUpStartPos.transform.position;
+        fadeScreen = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if(fadeScreen == true)
+        {
+            ///<<<<<<<<<<<<<<<<<<<<<fade screen to black>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+            ///
+
+            //load the rythm game scene.
+        }
+
         fishCaughtSlider.value = playerRodControl1.fishCaughtTotal + playerRodControl.fishCaughtTotal;
         if (playerRodControl1.fishCaughtTotal + playerRodControl.fishCaughtTotal > 10)
         {
@@ -63,6 +87,7 @@ public class fishBucketCounter : MonoBehaviour
             Invoke("hideCursedLootPopUp", 5);
 
             // loads of fish come out of water into the air
+            fishMovingUp.transform.position = Vector3.MoveTowards(fishMovingUp.transform.position, fishMovingUpEndPos.transform.position, 5 * Time.deltaTime);
 
 
             // water freezes over into ice
@@ -70,9 +95,22 @@ public class fishBucketCounter : MonoBehaviour
 
 
             //fish fall from sky onto ice (instantiate loads)
+            InstantiateFish();
+            Invoke("InstantiateFish", 1f);
+            Invoke("InstantiateFish", 2f);
+            Invoke("InstantiateFish", 3f);
+            Invoke("InstantiateFish", 4f);
+            Invoke("InstantiateFish", 5f);
+            Invoke("InstantiateFish", 6f);
+            Invoke("InstantiateFish", 7f);
+            Invoke("InstantiateFish", 8f);
+            Invoke("InstantiateFish", 9f);
+            Invoke("InstantiateFish", 10f);
 
+            //fade screen to black
+            Invoke("fadeToBlack", 15f);
 
-            //on fish spawn they either addforce with 0 friction OR bounce 
+         
 
         }
     }
@@ -84,4 +122,22 @@ public class fishBucketCounter : MonoBehaviour
         cursedLootUI.SetActive(false);
     }
 
+    public void InstantiateFish()
+    {
+        Instantiate(skyFish, skyFishSpawnLocation1.transform.position, skyFishSpawnLocation1.transform.rotation);
+        Instantiate(skyFish, skyFishSpawnLocation2.transform.position, skyFishSpawnLocation2.transform.rotation);
+        Instantiate(skyFish, skyFishSpawnLocation3.transform.position, skyFishSpawnLocation3.transform.rotation);
+        Instantiate(skyFish, skyFishSpawnLocation4.transform.position, skyFishSpawnLocation4.transform.rotation);
+        Instantiate(skyFish, skyFishSpawnLocation5.transform.position, skyFishSpawnLocation5.transform.rotation);
+        Instantiate(skyFish, skyFishSpawnLocation6.transform.position, skyFishSpawnLocation6.transform.rotation);
+        Instantiate(skyFish, skyFishSpawnLocation7.transform.position, skyFishSpawnLocation7.transform.rotation);
+        Instantiate(skyFish, skyFishSpawnLocation8.transform.position, skyFishSpawnLocation8.transform.rotation);
+    }
+
+    public void fadeToBlack()
+    {
+
+        fadeScreen = true;
+
+    }
 }
---
+++ Assets/fishingMiniGame/skyFish.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class skyFish : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //on fish spawn they either addforce with 0 friction OR bounce around
+    }
+}
